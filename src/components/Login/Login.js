80c2c50cf7528452c7c337de7040ecd765370963
nguyenducthao <ndthaodng@gmail.com>
2022-04-22 14:32:53 +0700

    video 9.3.1 taoj form login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,29 @@
+import './Login.scss'
 const Login = () => {
     return (
-        <div className="login-container">
-            login
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-7 red">
+                        <div className='brand'>
+                            PC3-INVEST
+                        </div>
+                        <div className='detail'>
+                            Programming...
+                        </div>
+                    </div>
+                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                        <input type='text' className='form-control' placeholder='Email address or phone number' />
+                        <input type='password' className='form-control' placeholder='Password' />
+                        <button className='btn btn-primary'>Login</button>
+                        <span className='text-center'>Forgot your password?</span>
+                        <hr />
+                        <div className='text-center'>
+                            <button className='btn btn-success'>Create new account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
