4bc5e4c8b8c28b1da559c6af43b7fe05898291fc
nguyenducthao <ndthaodng@gmail.com>
2022-05-18 16:15:52 +0700

    11.11 Bearer Authentication - Xac Thuc Voi Header Token

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -10,7 +10,7 @@ const instance = axios.create({
 instance.defaults.withCredentials = true
 
 // // Alter defaults after instance has been created
-// instance.defaults.headers.common['Authorization'] = 'AUTH_TOKEN 123456789';
+instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('jwt')}`;
 
 
 // Add a request interceptor
