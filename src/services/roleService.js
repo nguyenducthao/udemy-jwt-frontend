d30c58bb20308eefbfa4a3353d54e6c7f9657088
nguyenducthao <ndthaodng@gmail.com>
2022-05-23 18:07:29 +0700

    12.5 Display/Delete Roles - Hien Thi & Xoa Roles

--- src/services/roleService.js
+++ src/services/roleService.js
@@ -3,6 +3,14 @@ import axios from "../setup/axios"
 const createRoles = (roles) => {
     return axios.post("/api/v1/roles/create", [...roles])
 }
+const fetchAllRole = () => {
+    return axios.get("/api/v1/roles/read")
+}
+const deleteRole = (role) => {
+    return axios.delete("/api/v1/roles/delete", { data: { id: role.id } })
+}
 export {
-    createRoles
+    createRoles,
+    fetchAllRole,
+    deleteRole
 }
